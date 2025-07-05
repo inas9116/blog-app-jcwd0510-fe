dc0c8c7b90766dd8b62c636f6d4004679277a503
inas9116 <139898118+inas9116@users.noreply.github.com>
2025-07-05 19:38:51 +0700

    Merge pull request #1 from inas9116/register
    feat: register

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/link'
+
+const Navbar = () => {
+  return (
+    <nav className="bg-slate-400">
+      <div className="container mx-auto px-4">
+        <div className="flex items-center justify-between py-2">
+          <Link href="/">Logo</Link>
+
+          <div className="flex items-center gap-8 font-medium">
+            <Link href="/">Home</Link>
+            <Link href="/">Profile</Link>
+          </div>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar;
