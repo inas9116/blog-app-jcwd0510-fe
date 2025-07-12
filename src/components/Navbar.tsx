a479c510b9c2d462cb94ee1ee3ce23ebda9f43c7
inas9116 <139898118+inas9116@users.noreply.github.com>
2025-07-13 01:03:52 +0700

    feat: keep login (#3)

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,42 @@
-import Link from 'next/link'
+"use client";
+
+import { useAppDispatch, useAppSelector } from "@/redux/hooks";
+import { logoutAction } from "@/redux/slices/userSlice";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 const Navbar = () => {
+  const dispatch = useAppDispatch();
+  const router = useRouter();
+  const user = useAppSelector((state) => state.user);
+
+  const logout = () => {
+    localStorage.removeItem("blog-storage");
+    dispatch(logoutAction());
+    router.push("/login");
+  };
+
   return (
     <nav className="bg-slate-400">
       <div className="container mx-auto px-4">
         <div className="flex items-center justify-between py-2">
           <Link href="/">Logo</Link>
 
-          <div className="flex items-center gap-8 font-medium">
+          <div className="flex cursor-pointer items-center gap-8 font-medium">
             <Link href="/">Home</Link>
             <Link href="/">Profile</Link>
+            {/* {!user.id && <Link href="/login">login</Link>}
+            {!!user.id && <p onClick={logout}>Logout</p>} */}
+            {user.id ? (
+              <p onClick={logout}>logout</p>
+            ) : (
+              <Link href="/login">login</Link>
+            )}
           </div>
         </div>
       </div>
     </nav>
-  )
-}
+  );
+};
 
 export default Navbar;
--- src/hooks/api/auth/useLogin.ts
+++ src/hooks/api/auth/useLogin.ts
@@ -1,8 +1,11 @@
 "use client";
 
 import { axiosInstance } from "@/lib/axios";
+import { useAppDispatch } from "@/redux/hooks";
+import { loginAction } from "@/redux/slices/userSlice";
 import { useMutation } from "@tanstack/react-query";
 import { AxiosError } from "axios";
+import { useRouter } from "next/navigation";
 import { toast } from "react-toastify";
 
 interface LoginPayload {
@@ -10,13 +13,19 @@ interface LoginPayload {
   password: string;
 }
 const useLogin = () => {
+  const router = useRouter();
+  const dispatch = useAppDispatch();
+
   return useMutation({
     mutationFn: async (payload: LoginPayload) => {
       const { data } = await axiosInstance.post("/auth/login", payload);
       return data;
     },
-    onSuccess: () => {
+    onSuccess: (data) => {
       toast.success("Login succsess");
+      dispatch(loginAction(data)); //masukin data ke global state
+      localStorage.setItem("blog-storage", JSON.stringify(data)); // masukin data ke localStorage
+      router.replace("/");
     },
     onError: (error: AxiosError<any>) => {
       toast.error(error.response?.data);
