2ddea51d4272a757cfbddb0606b62f20d031dbe2
inas9116 <139898118+inas9116@users.noreply.github.com>
2025-07-15 16:01:14 +0700

    feat: forgot password (#4)

---
+++ src/hooks/api/auth/useForgotPassword.ts
@@ -0,0 +1,33 @@
+"use client";
+
+import { axiosInstance } from "@/lib/axios";
+import { useMutation } from "@tanstack/react-query";
+import { AxiosError } from "axios";
+import { useRouter } from "next/navigation";
+import { toast } from "react-toastify";
+
+interface ForgotPasswordPayload {
+  email: string;
+}
+
+const useForgotPassword = () => {
+  const router = useRouter();
+
+  return useMutation({
+    mutationFn: async (payload: ForgotPasswordPayload) => {
+      const { data } = await axiosInstance.post(
+        "/auth/forgot-password",
+        payload,
+      );
+      return data;
+    },
+    onSuccess: (data) => {
+      toast.success("check your email");
+      router.push("/");
+    },
+    onError: (error: AxiosError<any>) => {
+      toast.error(error.response?.data);
+    },
+  });
+};
+export default useForgotPassword;
